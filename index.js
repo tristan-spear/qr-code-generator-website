dbf8c64d9875b69d7a2b0354477e62a80d370230
tristan-spear <157447537+tristan-spear@users.noreply.github.com>
2025-09-28 11:27:36 -0700

    Finished basic website prototype

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import QRCode from 'qrcode';
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,7 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//app.set("view engine", "ejs");
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("home.ejs"); 
@@ -16,7 +16,18 @@ app.get("/", (req, res) => {
 
 app.post("/link-submit", (req, res) => {
     const link = req.body.link;
-    res.render();
+    console.log(link);
+
+    QRCode.toFile('./public/qr-file.png', link, {
+        color: {
+            dark: '#00F',  // Blue dots
+            light: '#0000' // Transparent background
+        }
+        }, function (err) {
+            if (err) throw err
+        console.log('done')
+    })
+    res.render("qr.ejs", {qrlink : link}); 
 })
 
 app.listen(port, () => {
