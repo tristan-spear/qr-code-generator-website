150ac5f77eb465c0eafdbfc73a71e9e78c20d150
tristan-spear <157447537+tristan-spear@users.noreply.github.com>
2025-09-28 16:28:45 -0700

    Small updates

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
     res.render("home.ejs"); 
 });
