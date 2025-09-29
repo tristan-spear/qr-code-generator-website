bb1744369f5825114139493e69b1ca36e89367ba
tristan-spear <157447537+tristan-spear@users.noreply.github.com>
2025-09-28 19:26:11 -0700

    deleting comments

--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-// import express from "express";
-// import QRCode from "qrcode";
-// import { fileURLToPath } from "url";
-// import { dirname, join } from "path";
-
-// const app = express();
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // View engine setup
-// app.set("view engine", "ejs");
-// app.set("views", join(__dirname, "views"));
-
-// // Routes
-// app.get("/", (req, res) => {
-//   res.render("home");
-// });
-
-// app.post("/link-submit", async (req, res) => {
-//   const link = req.body.link;
-
-//   try {
-//     const qrDataUrl = await QRCode.toDataURL(link, {
-//       color: {
-//         dark: "#000000",
-//         light: "#ffffffff",
-//       },
-//     });
-//     res.render("qr", { qrlink: link, qrDataUrl });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Error generating QR code.");
-//   }
-// });
-
-// export default app;
-
-// // app.listen(port, () => {
-// //   console.log(`Listening on port ${port}`);
-// // });
-
 import express from "express";
 import QRCode from 'qrcode';
 import bodyParser from "body-parser";
@@ -68,7 +22,7 @@ app.post("/link-submit", (req, res) => {
 
     QRCode.toFile('./public/qr-file.png', link, {
         color: {
-            dark: '#000000',  // Blue dots
+            dark: '#000000',  // Black dots
             light: '#ffffffff' // Transparent background
         }
         }, function (err) {
