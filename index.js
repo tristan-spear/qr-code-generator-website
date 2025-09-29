84f079112f5188853dd729a254decb6d94441945
tristan-spear <157447537+tristan-spear@users.noreply.github.com>
2025-09-28 19:08:25 -0700

    Adding and fixing

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 import express from "express";
-import QRCode from 'qrcode';
-import bodyParser from "body-parser";
-// import { fileURLToPath } from "url";
-// import { dirname, join } from "path";
+import QRCode from "qrcode";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const app = express();
-const port = 3000;
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// View engine setup
 app.set("view engine", "ejs");
-//app.set("views", join(__dirname, "views"));
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.set("views", join(__dirname, "views"));
 
+// Routes
 app.get("/", (req, res) => {
-    res.render("home"); 
+  res.render("home");
 });
 
-app.post("/link-submit", (req, res) => {
-    const link = req.body.link;
-    console.log(link);
+app.post("/link-submit", async (req, res) => {
+  const link = req.body.link;
 
-    QRCode.toFile('./public/qr-file.png', link, {
-        color: {
-            dark: '#000000',  // Blue dots
-            light: '#ffffffff' // Transparent background
-        }
-        }, function (err) {
-            if (err) throw err
-        console.log('done')
-    })
-    res.render("qr", {qrlink : link}); 
-})
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  try {
+    const qrDataUrl = await QRCode.toDataURL(link, {
+      color: {
+        dark: "#000000",
+        light: "#ffffffff",
+      },
+    });
+    res.render("qr", { qrlink: link, qrDataUrl });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error generating QR code.");
+  }
 });
 
-// export default app;
+export default app;
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
 
