e44f71a604daf5222f2e9031777fe445e8c05aec
tristan-spear <157447537+tristan-spear@users.noreply.github.com>
2025-09-28 18:58:09 -0700

    fixing and testing

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ app.post("/link-submit", (req, res) => {
     res.render("qr.ejs", {qrlink : link}); 
 })
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
 
-// export default app;
+export default app;
 
